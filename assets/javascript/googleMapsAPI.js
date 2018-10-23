dbe86018c56e10b455df9d088a74d349d2287e48
Tyler G <guthriet1230@gmail.com>
2018-10-22 20:56:18 -0500

    polished MVP

--- assets/javascript/googleMapsAPI.js
+++ assets/javascript/googleMapsAPI.js
@@ -2,65 +2,56 @@ console.log("test");
 
 var map;
 var infowindow;
-var beverage = $("#topic-input").val().trim();
-console.log('pre',beverage)
+var beverage = $("#topic-input")
+  .val()
+  .trim();
+console.log("pre", beverage);
 function initMap(beverage) {
-    console.log("init map");
-    var pyrmont = { lat: 41.881832, lng: -87.623177 };
+  console.log("init map");
+  var pyrmont = { lat: 41.881832, lng: -87.623177 };
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
-    });
-    console.log(map)
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: pyrmont,
+    zoom: 13.5
+  });
+  console.log(map);
 
-    infowindow = new google.maps.InfoWindow();
-    // console.log(google.maps.places.PlacesService);
-    console.log("THIS IS THE STRING" + google.maps.places);
-    var service = new google.maps.places.PlacesService(map);
-    console.log('post',beverage);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: 5000,
-        //type: ['store']
-        keyword: beverage,
-    }, callback);
+  infowindow = new google.maps.InfoWindow();
+  // console.log(google.maps.places.PlacesService);
+  console.log("THIS IS THE STRING" + google.maps.places);
+  var service = new google.maps.places.PlacesService(map);
+  console.log("post", beverage);
+  service.nearbySearch(
+    {
+      location: pyrmont,
+      radius: 10000,
+      type: ["bar"],
+      keyword: beverage
+    },
+    callback
+  );
 }
 
 function callback(results, status) {
-    console.log("CALLBACK")
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log("Callback Status OK")
-        for (var i = 0; i < results.length; i++) {
-            console.log("Creating Marker")
-            createMarker(results[i]);
-        }
+  console.log("CALLBACK");
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    console.log("Callback Status OK");
+    for (var i = 0; i < results.length; i++) {
+      console.log("Creating Marker");
+      createMarker(results[i]);
     }
+  }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
+  google.maps.event.addListener(marker, "click", function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
-
-
-// $("#add-topic").on("click", function () {
-//     event.preventDefault();
-//     console.log("i clicked this javascripttest button");
-  
-//     var drinkSelection = $("#topic-input").val().trim();
-//     $("#topic-input").val("");
-//     console.log(drinkSelection);
-//     //call the function
-//     initMap(drinkSelection);
-  
-  
-//   })
